936e4f9aed821e561f1096143b3f5b201fc428ef
dkm6083 <dkm6083@psu.edu>
2026-04-23 14:36:33 -0400

    Uploaded data
    Uploaded NFLVerse (player stats) data. This completes step 2.

--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -1,6 +1,15 @@
+# Step 1: Install and Load Packages ----
+install.packages("nflreadr")
+install.packages("nflreadr", repos = c("https://nflverse.r-universe.dev", getOption("repos")))
+remotes::install_github("nflverse/nflreadr")
+
 library(tidyverse)
+library(nflreadr)
 
-Combine_Performance <- read.csv("combine_official.csv")
-
-View(Combine_Performance)
-#
+# Step 2: Load Data ----
+# Combine_Performance <- read.csv("combine_official.csv")
+Player_Stats_2021 <- load_player_stats(2021)
+Player_Stats_2022 <- load_player_stats(2022)
+Player_Stats_2023 <- load_player_stats(2023)
+Player_Stats_2024 <- load_player_stats(2024)
+Player_Stats_2025 <- load_player_stats(2025)
