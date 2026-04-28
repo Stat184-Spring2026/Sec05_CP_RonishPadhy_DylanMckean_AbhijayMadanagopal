795830912b33a14113df07dc1c18906bf072996e
dkm6083 <dkm6083@psu.edu>
2026-04-28 17:40:39 -0400

    Cleaned Combine data
    Combined all combine data frames into one. Also found running back average three cone drill for specifically running backs

--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -21,3 +21,27 @@ Player_Stats_2022 <- load_player_stats(2022)
 Player_Stats_2023 <- load_player_stats(2023)
 Player_Stats_2024 <- load_player_stats(2024)
 Player_Stats_2025 <- load_player_stats(2025)
+
+# Step 3: Tidy Combine Data ----
+
+# Stack all combine years into one data frame
+Combine_All <- bind_rows(
+  Combine_2021,
+  Combine_2022,
+  Combine_2023,
+  Combine_2024,
+  Combine_2025
+)
+
+# Contextual note: Lower three cone time means player is
+# faster and more agile so a declining average means improvement.
+Combine_RB_Avg <- Combine_All |>
+  # Filter out all other positions besides running back
+  filter(pos == "RB") |> 
+  # Drop rows with no three cone performance  
+  filter(!is.na(cone)) |> 
+  group_by(season) |>
+  summarise(
+    # Find average time for three cone drill
+    avg_cone = mean(cone, na.rm = TRUE)
+  )
